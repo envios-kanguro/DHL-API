3d2937bd3b7c265a087cdfb2b7adda601ec8ee65
jessdelac <jessdelacruzb@gmail.com>
2019-07-31 18:08:10 -0500

    agregar formatos 8x4 y 6x4 PDF

--- DHL/Datatype/GB/Label.php
+++ DHL/Datatype/GB/Label.php
@@ -22,7 +22,8 @@
  * @version     0.1
  */
 
-namespace DHL\Datatype\GB; 
+namespace DHL\Datatype\GB;
+
 use DHL\Datatype\Base;
 
 /**
@@ -42,32 +43,32 @@ class Label extends Base
      */
     protected $_params = array(
         'LabelTemplate' => array(
-            'type' => 'LabelTemplate',
-            'required' => false,
-            'subobject' => false,
-            'comment' => 'LabelTemplate',
-            'enumeration' => '8X4_A4_PDF,8X4_thermal,8X4_A4_TC_PDF,6X4_A4_PDF,6X4_thermal,8X4_CI_PDF,8X4_CI_thermal',
-        ), 
-        'Logo' => array(
-            'type' => 'YesNo',
-            'required' => false,
-            'subobject' => false,
-            'comment' => 'Boolean flag',
-            'length' => '1',
+            'type'        => 'LabelTemplate',
+            'required'    => false,
+            'subobject'   => false,
+            'comment'     => 'LabelTemplate',
+            'enumeration' => '8X4_A4_PDF,8X4_thermal,8X4_A4_TC_PDF,6X4_A4_PDF,6X4_thermal,8X4_CI_PDF,8X4_CI_thermal,6X4_PDF,8X4_PDF',
+        ),
+        'Logo'          => array(
+            'type'        => 'YesNo',
+            'required'    => false,
+            'subobject'   => false,
+            'comment'     => 'Boolean flag',
+            'length'      => '1',
             'enumeration' => 'Y,N',
-        ), 
-        'CustomerLogo' => array(
-            'type' => 'CustomerLogo',
-            'required' => false,
+        ),
+        'CustomerLogo'  => array(
+            'type'      => 'CustomerLogo',
+            'required'  => false,
             'subobject' => true,
-        ), 
-        'Resolution' => array(
-            'type' => 'Resolution',
-            'required' => false,
-            'subobject' => false,
-            'comment' => 'Resolution',
+        ),
+        'Resolution'    => array(
+            'type'         => 'Resolution',
+            'required'     => false,
+            'subobject'    => false,
+            'comment'      => 'Resolution',
             'minInclusive' => '200',
             'maxInclusive' => '300',
-        ), 
+        ),
     );
 }
